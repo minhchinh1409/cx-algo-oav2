21b918d0bce0ae1af2f4b38933ea99e3c501a70f
MinhChinh <minhchinh140901@gmail.com>
2020-10-16 15:15:15 +0200

    Selection Sort

---
+++ e03.js
@@ -0,0 +1,21 @@
+function selectionSort(inputArr) { 
+    let n = inputArr.length;
+    while(!inputArr.sort());   
+    for(let i = 0; i < n; i++) {
+        // Finding the smallest number in the subarray
+        let min = i;
+        for(let j = 0; j < n; j++){
+            if(inputArr[j] < inputArr[min]) {
+                min=j; 
+            }
+         }
+         if (min = i) {
+             // Swapping the elements
+             let tmp = inputArr[i]; 
+             inputArr[i] = inputArr[min];
+             inputArr[min] = tmp;      
+        }
+    }
+    return inputArr;
+}
+console.log(selectionSort([4, 5, 9, 7]))
